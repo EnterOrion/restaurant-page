f56442f1ca13355aea8af6a805c7c99c387d9806
EnterOrion <orionwesson@gmail.com>
2022-12-23 20:12:58 +0100

    Add images to menu

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -17,6 +17,19 @@ function menuPage() {
     const item5 = createDiv("item5");
 
 
+    const image1 = createImage("image1", "images/latte.jpg", "A latte");
+    const image2 = createImage("image2", "images/iceLatte.jpg", "An iced latte");
+    const image3 = createImage("image3", "images/matcha.jpg", "A matcha latte");
+    const image4 = createImage("image4", "images/toast.jpg", "Toast with jam");
+    const image5 = createImage("image5", "images/cookies.jpg", "Plate of cookies");
+
+    item1.appendChild(image1);
+    item2.appendChild(image2);
+    item3.appendChild(image3);
+    item4.appendChild(image4);
+    item5.appendChild(image5);
+    
+
     content.appendChild(menuHeader);
     content.appendChild(drinkHeader);
     menuItems.appendChild(item1);
@@ -32,6 +45,14 @@ function createDiv(id) {
     const divItem = document.createElement("div");
     divItem.setAttribute('id', id);
     return divItem;
+}
+
+function createImage(id, src, alt) {
+    const imgItem = document.createElement("img");
+    imgItem.setAttribute('id', id);
+    imgItem.src = src;
+    imgItem.alt = alt;
+    return imgItem;
 }
 
 
