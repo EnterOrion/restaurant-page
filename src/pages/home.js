1c1af35e7e95e4089dba0e463bfde6d613e26507
EnterOrion <orionwesson@gmail.com>
2022-12-23 14:14:02 +0100

    Basic button style; style main pic and text

--- src/pages/home.js
+++ src/pages/home.js
@@ -19,7 +19,7 @@ function homePage(){
     const content = document.querySelector('#content');
     const titleHeader = document.createElement("h1");
     titleHeader.textContent = "Cosmic Time Cafe";
-    const infoPara = createParagraph("Have you ever been to a cafe that changed your life? Cosmic Time Cafe strives to offer the optimal cafe experience. We have a view of the milky way out of our window and we are a very popular choice for time travelers to visit. Interested in what the future looks like? Stop on by!");
+    const infoPara = createParagraph("Have you ever been to a cafe that changed your life? Cosmic Time Cafe strives to offer the optimal cafe experience. We have a view of the milky way out of our window and we are a very popular choice for time travelers to visit. Interested in what the future looks like? Stop on by today!");
 
     content.appendChild(nav);
     content.appendChild(titleHeader);
