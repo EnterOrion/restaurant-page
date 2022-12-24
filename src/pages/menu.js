cbe8aa7e72404ccf9f7f22b4678c23c31b7b85c5
EnterOrion <orionwesson@gmail.com>
2022-12-24 14:25:27 +0100

    Add caption and style to menu items

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -3,8 +3,8 @@ function menuPage() {
     const content = document.querySelector('#main');
     content.textContent = "";
     const menuHeader = document.createElement("h2");
-    const foodHeader = document.createElement("h4");
-    const drinkHeader = document.createElement("h4");
+    const foodHeader = document.createElement("h3");
+    const drinkHeader = document.createElement("h3");
 
     menuHeader.textContent = "Menu";
     foodHeader.textContent = "Food";
@@ -28,16 +28,26 @@ function menuPage() {
     item3.appendChild(image3);
     item4.appendChild(image4);
     item5.appendChild(image5);
-    
+
+    const caption1 = createCaption("caption1", "Star Foam Latte - $5");
+    const caption2 = createCaption("caption2", "Iced Latte with Milky Way Cream - $5.5");
+    const caption3 = createCaption("caption3", "Earthy Matcha Latte - $6");
+    const caption4 = createCaption("caption4", "Toast with choice of jam - $3");
+    const caption5 = createCaption("caption5", "Freshly baked cookies - $5");
 
     content.appendChild(menuHeader);
     content.appendChild(drinkHeader);
     menuItems.appendChild(item1);
+    menuItems.appendChild(caption1);
     menuItems.appendChild(item2);
+    menuItems.appendChild(caption2);
     menuItems.appendChild(item3);
+    menuItems.appendChild(caption3);
     menuItems.appendChild(foodHeader);
     menuItems.appendChild(item4);
+    menuItems.appendChild(caption4);
     menuItems.appendChild(item5);
+    menuItems.appendChild(caption5);
     content.appendChild(menuItems);
 }
 
@@ -53,6 +63,13 @@ function createImage(id, src, alt) {
     imgItem.src = src;
     imgItem.alt = alt;
     return imgItem;
+}
+
+function createCaption(id, text) {
+    const captionItem = document.createElement("div");
+    captionItem.setAttribute('id', id);
+    captionItem.textContent = text;
+    return captionItem;
 }
 
 
